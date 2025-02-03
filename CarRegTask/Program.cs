24f1cdc7256075a37d5ee0b8c74d5bfa3470a9c8
Tanush Chetan <22tchetan@heckgrammar.co.uk>
2025-02-03 21:44:14 +0000

    BETTER

--- CarRegTask/Program.cs
+++ CarRegTask/Program.cs
@@ -5,15 +5,15 @@
         static void Main(string[] args)
         {
             int charge = 0;
-            Console.Write("Enter your car registration: ");
+            Console.WriteLine("Enter your car registration: ");
             string carReg = Console.ReadLine();
             while (carReg.Length > 8)
             {
                 string displayMessge = carReg + " is not valid";
-                Console.Write(displayMessge);
+                Console.WriteLine(displayMessge);
                 carReg = Console.ReadLine();
             }
-            Console.Write("Enter your stay in hours: ");
+            Console.WriteLine("Enter your stay in hours: ");
             int hours = Convert.ToInt32(Console.ReadLine());
             if (hours < 2)
             {
